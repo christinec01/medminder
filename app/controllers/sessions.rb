aff6d54726df90ec4c9aa492fb644d77b20308ba
Christine Casebolt <christine.casebolt@gmail.com>
2016-10-18 20:50:10 -0700

    merge conflicts

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 post '/sessions' do
   user = User.find_by(email: params[:user][:email])
   # binding.pry
@@ -6,7 +6,7 @@ post '/sessions' do
       session[:user_id] = user.id
       p "* in if" *10
       redirect '/user/:id/med'
-=======
+
 get '/sessions/new' do
   redirect 'med_minder/show'
 end
@@ -17,7 +17,7 @@ post '/med_minder' do
     if user && User.authenticate(user.email, params[:password])
       session[:user_id] = user.id
       redirect 'med_minder/show'
->>>>>>> ced8a7edf7e9f9762b2dcff3e376834158cfee67
+
     else
       p "* in else" *10
       @errors = "Email and Password not found. Please try again."
