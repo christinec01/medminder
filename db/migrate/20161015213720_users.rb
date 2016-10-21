923edffe17a375eec6eb24adc7e3db346aa92bec
Christine Casebolt <christine.casebolt@gmail.com>
2016-10-21 13:57:41 -0700

    WORKING APP

--- db/migrate/20161015213720_users.rb
+++ db/migrate/20161015213720_users.rb
@@ -2,7 +2,7 @@ class Users < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email, :null => false
-      t.integer :phone_number, :null => false
+      t.string :phone_number, :null => false
       t.string :password_digest, :null => false
     end
   end
--- db/migrate/20161020163357_medreminders.rb
+++ db/migrate/20161020163357_medreminders.rb
@@ -2,7 +2,7 @@ class Medreminders < ActiveRecord::Migration
   def change
   create_table :med_reminders do |t|
       t.string :name
-      t.integer :phone_number
+      t.string :phone_number
       t.string :time
 
     end
